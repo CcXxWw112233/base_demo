bfcf6ae81812ded1568240df0468a1941c44bd37
xuwei.chen <xuwei.chen@qizhidao.com>
2022-02-10 14:02:14 +0800

    d3

---
+++ d3js/1.js
@@ -0,0 +1,9 @@
+d3.select("body")
+  .append("svg")
+    .attr("width", 960)
+    .attr("height", 500)
+  .append("g")
+    .attr("transform", "translate(20,20)")
+  .append("rect")
+    .attr("width", 920)
+    .attr("height", 460);
